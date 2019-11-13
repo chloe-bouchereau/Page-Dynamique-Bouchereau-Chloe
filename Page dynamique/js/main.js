d3d0504966720c838f2fdb91c7be25f6c92fa49b
chloe-bouchereau <bouchereau.chloe27012000@gmail.com>
2019-11-13 16:47:57 +0100

    New

---
+++ Page dynamique/js/main.js
@@ -0,0 +1,66 @@
+
+var menu = document.getElementById('menu');
+var dropdown = document.getElementById('dropbutton');
+var visible = false;
+var espace = false;
+
+
+/*Menu hamburger*/
+if(visible == false){
+    menu.addEventListener('click', function button(){
+        if(visible == false){
+        visible = true;
+        document.getElementById("hamburger").style.display = "block";
+        document.getElementById("content_section").classList.add("deplacement"); 
+         
+    }
+        else{
+            visible = false;
+            document.getElementById("hamburger").style.display = "none";
+            document.getElementById("content_section").classList.remove("deplacement"); 
+            
+
+        }
+})};
+
+
+
+
+/*Dropdown*/ 
+if(espace == false){
+        dropdown.addEventListener("click", function button_espace(){
+        if(espace == false){
+        espace = true;
+        document.getElementById("dropcontent").style.display ="block";
+
+    }
+        else{
+            espace = false;
+            document.getElementById("dropcontent").style.display = "none";
+        }
+})};
+
+
+/*Animation scroll/navbar */ 
+
+window.onscroll = function(){
+
+    var scroll = window.scrollY;
+    console.log(scroll);
+    if(scroll >= 15){
+        document.getElementById("barre-nav").classList.add("fade-out");
+        console.log('hello');
+    }
+    else{
+        document.getElementById("barre-nav").classList.add("fade-in");
+        document.getElementById("barre-nav").classList.remove("fade-out");
+        console.log('hello2');
+    }
+
+};
+
+/* Animation appartion Titre*/ 
+
+document.getElementById("Hello").classList.add("fade-in");
+
+
